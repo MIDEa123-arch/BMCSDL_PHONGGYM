b91c6cc186f493dd4246409ff6449786d93e1ce8
thankman100505-prog <thankman1005@gmail.com>
2025-12-04 00:31:20 +0700

    mahoades

--- QL_GYM/UserApp/Helpers/MaHoa.cs
+++ QL_GYM/UserApp/Helpers/MaHoa.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using UserApp.Helpers;
@@ -40,6 +42,28 @@ namespace UserApp.Helpers
                 result.Add(ListChar[newIndex]);
             }
             return new string(result.ToArray());
+        }
+        public static string MahoaDes(string input, int k)
+        {
+            byte[] keyBytes = new byte[8];
+            byte[] kBytes = BitConverter.GetBytes(k);
+            for (int i = 0; i < 8; i++)
+                keyBytes[i] = (i < kBytes.Length) ? kBytes[i] : (byte)0;
+
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                des.Key = keyBytes;
+                des.IV = keyBytes;
+
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(inputBytes, 0, inputBytes.Length);
+                    cs.FlushFinalBlock();
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
         }
     }
 }
